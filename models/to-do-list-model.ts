2c89a04f9d6faebb08ec1a0fe1ed1ff675038127
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2021-06-08 18:30:20 +0200

    Update to-do-list-model.ts

--- models/to-do-list-model.ts
+++ models/to-do-list-model.ts
@@ -1,13 +1,27 @@
 import {ItemModel} from "./item-model";
 
 interface ToDoListModelProps {
+    name: string;
+    description: string;
     list: ItemModel[];
 }
 
 export class ToDoListModel implements ToDoListModelProps {
+    private _name: string;
+    private _description: string;
     list: ItemModel[];
 
     constructor() {
         this.list = [];
+        this._name = "";
+        this._description = "";
     }
+
+    set description(value: string) {
+        this._description = value;
+    }
+    set name(value: string) {
+        this._name = value;
+    }
+
 }
